b84f1693fbfc6942c1a6ceff4e37f1ff5c836d45
Alex Nichol <unixpickle@gmail.com>
2018-12-04 11:49:57 -0500

    fix errors & implement queue+buffers

--- fake_webcam/fake_webcam.c
+++ fake_webcam/fake_webcam.c
@@ -1,6 +1,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/sched.h>
 #include <linux/string.h>
 #include <media/v4l2-dev.h>
 #include <media/v4l2-device.h>
@@ -15,11 +16,14 @@ MODULE_VERSION("0.01");
 
 struct fw_info {
   struct mutex ioctl_lock;
-  struct mutex queue_lock;
 
   struct video_device* dev;
   struct v4l2_device parent_dev;
   struct vb2_queue queue;
+
+  // Tracking frame timing.
+  int is_first_frame;
+  u64 next_time;
 };
 
 static struct fw_info fw_info;
@@ -77,11 +81,12 @@ static int fw_vidioc_querycap(struct file* f,
 static int fw_vidioc_enum_fmt_vid_cap(struct file* f,
                                       void* priv,
                                       struct v4l2_fmtdesc* fmt) {
-  if (f->index) {
+  if (fmt->index) {
     return -EINVAL;
   }
-  strncpy(fmt->description, fw_fmt_description, sizeof(f->description));
+  strncpy(fmt->description, fw_fmt_description, sizeof(fmt->description));
   fmt->pixelformat = fw_fmt_pixelformat;
+  return 0;
 }
 
 static int fw_vidioc_g_fmt_vid_cap(struct file* f,
@@ -103,64 +108,70 @@ static int fw_vidioc_try_fmt_vid_cap(struct file* f,
   if (fmt->fmt.pix.pixelformat != fw_fmt_pixelformat) {
     return -EINVAL;
   }
-  return fw_vidioc_g_fmt_vid_cap(f, priv, f);
+  return fw_vidioc_g_fmt_vid_cap(f, priv, fmt);
 }
 
-static int vidioc_s_std(struct file* f, void* priv, v4l2_std_id* id) {
+static int fw_vidioc_s_std(struct file* f, void* priv, v4l2_std_id id) {
   return 0;
 }
 
-static int vidioc_enum_input(struct file* f,
-                             void* priv,
-                             struct v4l2_input* input) {
-  if (inp->index) {
+static int fw_vidioc_enum_input(struct file* f,
+                                void* priv,
+                                struct v4l2_input* input) {
+  if (input->index) {
     return -EINVAL;
   }
-  inp->type = V4L2_INPUT_TYPE_CAMERA;
-  inp->std = fw_fmt_std;
-  strncpy(inp->name, "Fake Webcam", sizeof(inp->name));
+  input->type = V4L2_INPUT_TYPE_CAMERA;
+  input->std = fw_fmt_std;
+  strncpy(input->name, "Fake Webcam", sizeof(input->name));
   return 0;
 }
 
-static int vidioc_g_input(struct file* f, void* priv, unsigned int* i) {
+static int fw_vidioc_g_input(struct file* f, void* priv, unsigned int* i) {
   *i = 0;
   return 0;
 }
 
-static int vidioc_s_input(struct file* f, void* priv, unsigned int i) {
+static int fw_vidioc_s_input(struct file* f, void* priv, unsigned int i) {
   if (i) {
     return -EINVAL;
   }
   return 0;
 }
 
-static int vidioc_reqbufs(struct file* f,
-                          void* priv,
-                          struct v4l2_requestbuffers* req) {
+static int fw_vidioc_reqbufs(struct file* f,
+                             void* priv,
+                             struct v4l2_requestbuffers* req) {
   return vb2_reqbufs(&fw_info.queue, req);
 }
 
-static int vidioc_querybuf(struct file* f,
-                           void* priv,
-                           struct v4l2_buffer* buffer) {
+static int fw_vidioc_querybuf(struct file* f,
+                              void* priv,
+                              struct v4l2_buffer* buffer) {
   return vb2_querybuf(&fw_info.queue, buffer);
 }
 
-static int vidioc_qbuf(struct file* f, void* priv, struct v4l2_buffer* buffer) {
+static int fw_vidioc_qbuf(struct file* f,
+                          void* priv,
+                          struct v4l2_buffer* buffer) {
   return vb2_qbuf(&fw_info.queue, buffer);
 }
 
-static int vidioc_dqbuf(struct file* f,
-                        void* priv,
-                        struct v4l2_buffer* buffer) {
-  return vb2_dqbuf(&dev->vb_vidq, buffer);
+static int fw_vidioc_dqbuf(struct file* f,
+                           void* priv,
+                           struct v4l2_buffer* buffer) {
+  return vb2_dqbuf(&fw_info.queue, buffer, f->f_flags & O_NONBLOCK);
 }
 
-static int vidioc_streamon(struct file* f, void* priv, enum v4l2_buf_type t) {
+static int fw_vidioc_streamon(struct file* f,
+                              void* priv,
+                              enum v4l2_buf_type t) {
   return vb2_streamon(&fw_info.queue, t);
 }
 
-static int vidioc_streamoff(struct file* f, void* priv, enum v4l2_buf_type t) {
+static int fw_vidioc_streamoff(struct file* f,
+                               void* priv,
+                               enum v4l2_buf_type t) {
   return vb2_streamoff(&fw_info.queue, t);
 }
 
@@ -181,8 +192,8 @@ static struct v4l2_ioctl_ops fw_ioctl_ops = {
     .vidioc_querybuf = fw_vidioc_querybuf,
     .vidioc_qbuf = fw_vidioc_qbuf,
     .vidioc_dqbuf = fw_vidioc_dqbuf,
-    .vidioc_streamon = vidioc_streamon,
-    .vidioc_streamoff = vidioc_streamoff,
+    .vidioc_streamon = fw_vidioc_streamon,
+    .vidioc_streamoff = fw_vidioc_streamoff,
 };
 
 // Device operations
@@ -195,8 +206,75 @@ static void fw_video_device_release(struct video_device* dev) {
 
 // Video buffer operations
 
-static struct vb2_ops fw_vb2_ops = {
+static int fw_vb2_queue_setup(struct vb2_queue* queue,
+                              unsigned int* nbuffers,
+                              unsigned int* nplanes,
+                              unsigned int sizes[],
+                              struct device* alloc_devs[]) {
+  *nbuffers = 4;
+  *nplanes = 1;
+  sizes[0] = (fw_fmt_width * fw_fmt_height * fw_fmt_depth) / 8;
+  return 0;
+}
 
+static void fw_vb2_wait_prepare(struct vb2_queue* queue) {
+  mutex_unlock(&fw_info.ioctl_lock);
+}
+
+static void fw_vb2_wait_finish(struct vb2_queue* queue) {
+  mutex_lock(&fw_info.ioctl_lock);
+}
+
+static int fw_vb2_start_streaming(struct vb2_queue* queue, unsigned int count) {
+  fw_info.is_first_frame = 1;
+  return 0;
+}
+
+static void fw_vb2_stop_streaming(struct vb2_queue* queue) {
+  vb2_wait_for_all_buffers(queue);
+}
+
+static u64 fw_get_nanotime(void) {
+  struct timeval val;
+  do_gettimeofday(&val);
+  return (u64)val.tv_sec * 1000000000 + (u64)val.tv_usec * 1000;
+}
+
+static int fw_ship_buffer_thread(void* buf_void) {
+  struct vb2_buffer* buffer = (struct vb2_buffer*)buf_void;
+
+  u64 nanotime = fw_get_nanotime();
+  if (nanotime < buffer->timestamp) {
+    schedule_timeout_interruptible(
+        nsecs_to_jiffies64(buffer->timestamp - nanotime));
+  }
+
+  vb2_buffer_done(buffer, VB2_BUF_STATE_DONE);
+
+  do_exit(0);
+  return 0;
+}
+
+static void fw_vb2_buf_queue(struct vb2_buffer* buffer) {
+  u64 nanotime = fw_get_nanotime();
+  if (fw_info.is_first_frame) {
+    fw_info.is_first_frame = 0;
+    fw_info.next_time = nanotime;
+  } else if (fw_info.next_time < nanotime) {
+    // The frames aren't being consumed fast enough.
+    fw_info.next_time = nanotime;
+  }
+  buffer->timestamp = fw_info.next_time;
+  kthread_run(fw_ship_buffer_thread, buffer, "bufqueue");
+}
+
+static struct vb2_ops fw_vb2_ops = {
+    .queue_setup = fw_vb2_queue_setup,
+    .wait_prepare = fw_vb2_wait_prepare,
+    .wait_finish = fw_vb2_wait_finish,
+    .start_streaming = fw_vb2_start_streaming,
+    .stop_streaming = fw_vb2_stop_streaming,
+    .buf_queue = fw_vb2_buf_queue,
 };
 
 // Module lifecycle
@@ -206,7 +284,6 @@ static int __init fw_init(void) {
 
   memset(&fw_info, 0, sizeof(fw_info));
   mutex_init(&fw_info.ioctl_lock);
-  mutex_init(&fw_info.queue_lock);
 
   strncpy(fw_info.parent_dev.name, "fake_webcam",
           sizeof(fw_info.parent_dev.name));
@@ -218,7 +295,6 @@ static int __init fw_init(void) {
   fw_info.queue.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
   fw_info.queue.io_modes = VB2_MMAP | VB2_USERPTR | VB2_READ;
   fw_info.queue.dev = fw_info.parent_dev.dev;
-  fw_info.queue.lock = &fw_info.queue_lock;
   fw_info.queue.ops = &fw_vb2_ops;
   fw_info.queue.mem_ops = &vb2_vmalloc_memops;
   fw_info.queue.buf_struct_size = sizeof(struct vb2_buffer);

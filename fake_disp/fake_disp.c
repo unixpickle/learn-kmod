131b6cbf2deb7cffa907f88c49381648713500ed
Alex Nichol <unixpickle@gmail.com>
2018-12-06 13:25:55 -0500

    add an extra log

--- fake_disp/fake_disp.c
+++ fake_disp/fake_disp.c
@@ -320,8 +320,8 @@ static int __init fake_disp_init(void) {
     goto fail_5;
   }
 
-  printk("fake_disp num connector %d\n",
-         state.device->mode_config.num_connector);
+  printk("fake_disp num connector %d (conn status should be 1 %d)\n",
+         state.device->mode_config.num_connector, state.connector.status);
   state.fbdev = drm_fbdev_cma_init(state.device, 24, 1);
   drm_kms_helper_poll_init(state.device);
 

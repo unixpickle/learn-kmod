2d41f667fa2b9292e9bbba98c2ad104b0bb71331
Alex Nichol <unixpickle@gmail.com>
2018-12-06 12:37:47 -0500

    fix err

--- fake_disp/fake_disp.c
+++ fake_disp/fake_disp.c
@@ -3,6 +3,7 @@
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_encoder.h>
+#include <drm/drm_fb_cma_helper.h>
 #include <drm/drm_gem.h>
 #include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_gem_framebuffer_helper.h>

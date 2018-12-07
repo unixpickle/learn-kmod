6716fd30f72acd38addcfcb755b872e92b61892a
Alex Nichol <unixpickle@gmail.com>
2018-12-07 09:08:27 -0500

    split up into files

---
+++ fake_disp/fake_disp_main.c
@@ -0,0 +1,27 @@
+#include "fake_disp.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Alex Nichol");
+MODULE_DESCRIPTION("A fake external monitor.");
+MODULE_VERSION("0.01");
+
+static struct fake_disp_state state;
+
+struct fake_disp_state* fake_disp_get_state(void) {
+  return &state;
+}
+
+extern unsigned int drm_debug;
+
+static int __init fake_disp_init(void) {
+  // Enable this to see a ton of log messages.
+  drm_debug = 0xffffffff;
+  return fake_disp_setup_drm();
+}
+
+static void __exit fake_disp_exit(void) {
+  fake_disp_destroy_drm();
+}
+
+module_init(fake_disp_init);
+module_exit(fake_disp_exit);

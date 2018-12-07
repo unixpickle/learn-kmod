7af636be1b2083373db91e677b1f59c62dae0074
Alex Nichol <unixpickle@gmail.com>
2018-12-07 10:25:07 -0500

    drafted all but mmap

--- fake_disp/fake_disp_mm.c
+++ fake_disp/fake_disp_mm.c
@@ -1,4 +1,114 @@
+#include <linux/gfp.h>
 #include "fake_disp.h"
+
+struct fake_disp_page {
+  struct page* page;
+  list_head list;
+};
+
+struct fake_disp_gem_object struct {
+  struct drm_gem_object base;
+  struct list_head pages;
+};
+
+static void fake_disp_gem_free_pages(struct fake_disp_gem_object* obj) {
+  struct list_head* next_page = obj->pages.next;
+  while (next_page != &obj->pages) {
+    struct fake_disp_page* page =
+        container_of(next_page, struct fake_disp_page, list);
+    next_page = next_page->next;
+    __free_page(page->page);
+    kfree(page);
+  }
+  INIT_LIST_HEAD(&obj->pages);
+}
+
+static int fake_disp_gem_alloc_pages(struct fake_disp_gem_object* obj,
+                                     size_t size) {
+  size_t total = 0;
+  while (total < size) {
+    struct page* raw_page = alloc_page();
+    if (!raw_page) {
+      fake_disp_gem_free_pages(obj);
+      return -ENOMEM;
+    }
+    struct fake_disp_page* page = kmalloc(sizeof(struct fake_disp_page), 0);
+    if (!page) {
+      __free_page(raw_page);
+      fake_disp_gem_free_pages(obj);
+      return -ENOMEM;
+    }
+    page->page = page;
+    list_add(&page->list, &obj.pages);
+    total += PAGE_SIZE;
+  }
+  return 0;
+}
+
+static struct drm_gem_object* fake_disp_gem_create(
+    struct drm_device* dev,
+    struct drm_mode_create_dumb* args) {
+  printk(KERN_INFO "fake_disp gem_dumb_create (pid=%d)\n",
+         task_pid_nr(current));
+  struct fake_disp_gem_object* obj;
+  int res;
+
+  unsigned int min_pitch = DIV_ROUND_UP(args->width * args->bpp, 8);
+  if (args->pitch < min_pitch) {
+    args->pitch = min_pitch;
+  }
+  if (args->size < args->pitch * args->height) {
+    args->size = args->pitch * args->height;
+  }
+
+  obj = kmalloc(sizeof(struct fake_disp_gem_object), 0);
+  res = drm_gem_object_init(dev, &obj->base, args->size);
+  if (res) {
+    goto fail_1;
+  }
+
+  res = drm_gem_create_mmap_offset(&obj->base);
+  if (res) {
+    goto fail_2;
+  }
+
+  res = fake_disp_gem_alloc_pages(obj, args->size);
+  if (res) {
+    goto fail_3;
+  }
+
+  return &obj->base;
+
+fail_3:
+  drm_gem_free_mmap_offset(&obj->base);
+fail_2:
+  drm_gem_object_release(&obj->base);
+fail_1:
+  kfree(obj);
+
+  return ERR_PTR(res);
+}
+
+static int fask_disp_gem_create_handle(struct drm_file* file_priv,
+                                       struct drm_device* dev,
+                                       struct drm_mode_create_dumb* args,
+                                       u32* handle) {
+  int res;
+
+  struct drm_gem_object* obj = fake_disp_gem_create(dev, args);
+  if (IS_ERR(obj)) {
+    return PTR_ERR(obj);
+  }
+
+  res = drm_gem_handle_create(file_priv, obj, handle);
+  if (res) {
+    fake_disp_gem_free_object(obj);
+    return res;
+  }
+
+  drm_gem_object_put_unlocked(obj);
+  return 0;
+}
 
 int fake_disp_mmap(struct file* filp, struct vm_area_struct* vma) {
   printk(KERN_INFO "fake_disp mmap(%lu) (pid=%d)\n", vma->vm_pgoff,
@@ -6,12 +116,12 @@ int fake_disp_mmap(struct file* filp, struct vm_area_struct* vma) {
   return drm_gem_cma_mmap(filp, vma);
 }
 
-int fake_disp_gem_dumb_create(struct drm_file* file,
+int fake_disp_gem_dumb_create(struct drm_file* file_priv,
                               struct drm_device* dev,
                               struct drm_mode_create_dumb* args) {
   printk(KERN_INFO "fake_disp gem_dumb_create (pid=%d)\n",
          task_pid_nr(current));
-  return drm_gem_cma_dumb_create(file, dev, args);
+  return fake_disp_gem_create_handle(file_priv, dev, args, &args > handle);
 }
 
 int fake_disp_gem_dumb_map_offset(struct drm_file* file,
@@ -27,12 +137,16 @@ int fake_disp_gem_dumb_destroy(struct drm_file* file_priv,
                                struct drm_device* dev,
                                uint32_t handle) {
   printk(KERN_INFO "fake_disp gem_dumb_destroy\n");
-  return 0;
+  return drm_gem_dumb_destroy(file_priv, dev, handle);
 }
 
 void fake_disp_gem_free_object(struct drm_gem_object* gem_obj) {
-  printk(KERN_INFO "fake_disp gem_free_object\n");
-  drm_gem_cma_free_object(gem_obj);
+  struct fake_disp_gem_object* obj =
+      container_of(gem_obj, struct fake_disp_gem_object, base);
+  fake_disp_gem_free_pages(obj);
+  drm_gem_free_mmap_offset(gem_obj);
+  drm_gem_object_release(gem_obj);
+  kfree(obj);
 }
 
 struct drm_framebuffer* fake_disp_user_framebuffer_create(
